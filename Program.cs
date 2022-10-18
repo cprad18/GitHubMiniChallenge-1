ee9f9eb4456cae53165dd5ee5fd65bc2095cdf71
Christopher Prado <cprad18@gmail.com>
2022-10-18 15:39:18 -0700

    2nd push

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Christopher Prado
+//09-07-2022 2pm
+//Mini Challenge #1 "What is your name?"
+//This is a console project that will ask our user their name
+//And report it back to the console
+//Peer Review by: (enter name)
+
+
+Console.Clear ();
+
+
+Console.WriteLine ("What is your name?");
+
+string myName = Console.ReadLine();
+
+Console.WriteLine ("Good Afternoon" + myName);
+
+Console.WriteLine ("What time did you wake up?");
+
+string myWakeTime = Console.ReadLine();
+
+Console.WriteLine ("Awesome " +  myName + " you woke up at " + myWakeTime);
